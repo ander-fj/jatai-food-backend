4e800213b4733e4368a9040f019ae79b7c003564
Boni0072 <ander.fj100@gmail.com>
2025-11-13 11:08:06 -0300

    Initial commit - Backend Jatai Food

---
+++ server2.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/api/test', (req, res) => {
+  res.send('Test route is working!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
